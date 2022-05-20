1667ea0a23a10a7fcccfaaf283cee458096eccf4
Alone <34532101+alonelove7@users.noreply.github.com>
2022-05-20 12:45:55 +0430

    Update main.py

--- main.py
+++ main.py
@@ -535,20 +535,20 @@ def onmessage(update,bot:ObigramClient):
             ddl(update,bot,message,url,file_name='',thread=thread,jdb=jdb)
         else:
             #if update:
-            #    api_id = os.environ.get('api_id')
-            #    api_hash = os.environ.get('api_hash')
-            #    bot_token = os.environ.get('bot_token')
-            #    
+                api_id = os.environ.get('api_id')
+                api_hash = os.environ.get('api_hash')
+                bot_token = os.environ.get('bot_token')
+                
                 # set in debug
-            #    api_id = 7386053
-            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+               api_id = 16612056
+               api_hash = 'ae32caf162207865ff93b9b931b2ba54'
+               bot_token = '5320935817:AAGr6lHU75b66XHyPmfFcI390McdZAzojeE'
 
-            #    chat_id = int(update.message.chat.id)
-            #    message_id = int(update.message.message_id)
-            #    import asyncio
-            #    asyncio.run(tlmedia.download_media(api_id,api_hash,bot_token,chat_id,message_id))
-            #    return
+                chat_id = int(update.message.chat.id)
+                message_id = int(update.message.message_id)
+                import asyncio
+                asyncio.run(tlmedia.download_media(api_id,api_hash,bot_token,chat_id,message_id))
+                return
             bot.editMessageText(message,'😵No se pudo procesar😵')
     except Exception as ex:
            print(str(ex))
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    #bot_token = '5270983050:AAF9-0hePS70oBWP4bmCZaQZUXfOvyhI1hA'
+    bot_token = '5320935817:AAGr6lHU75b66XHyPmfFcI390McdZAzojeE'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
